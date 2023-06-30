3065949623fbd2d8b9ce4e37d3a82056a7fcc6ce
Banegas , Agustina <133693422+BanegasAgustina@users.noreply.github.com>
2023-06-30 19:16:09 -0300

    Add files via upload

---
+++ primis.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+ bool EsPrimo(int n);
+int main(){
+	int num;
+	cout<<"ingrese un numero";
+	cin>>num; 
+	bool b= EsPrimo(num);
+	if(b==true){
+		cout<<"es primo";
+	}
+	if(b==false){
+		cout<<"no es primo";
+	}
+	return 0;
+}
+ bool EsPrimo(int n){ 
+ bool flag=false;
+ if(n%n==0||n%1==0){
+ 	flag=true;
+ }
+ return flag;
+ }
